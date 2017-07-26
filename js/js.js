10045ead3c5fda4cc05230758cf0a54dec8d5eb8
Jesse Smith <JesseSmith3rd@outlook.com>
2017-07-26 15:17:56 -0400

    New updates

--- js/js.js
+++ js/js.js
@@ -1,2 +1,10 @@
 alert("Welcome to a website that covers nothing but GTO")
 prompt('Please enter first name here to enter website')
+
+var myName = "Jesse Smith";
+var myAge = 39;
+
+var x,y,z;
+var z;
+var y;
+console.log(myName);
