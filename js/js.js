dd3b1f790890647ce6a1b2d97cc01e39c3c2e308
Jesse Smith <JesseSmith3rd@outlook.com>
2017-07-28 21:33:55 -0400

    Removed prompt and alert on webpage

--- js/js.js
+++ js/js.js
@@ -1,5 +1,4 @@
-alert("Welcome to a website that covers nothing but GTO")
-prompt('Please enter first name here to enter website')
+
 
 var myName = "Jesse Smith";
 var myAge = 39;
@@ -7,4 +6,6 @@ var myAge = 39;
 var x,y,z;
 var z;
 var y;
+
+yourName = "BOB";
 console.log(myName);
